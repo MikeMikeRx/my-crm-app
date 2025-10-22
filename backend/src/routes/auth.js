30975ca7977afccee06568ba039c39f529232ba8
MichalRais <Michal.Rais@iCloud.com>
2025-10-22 07:33:40 +0100

    feat: Define and export core authentication routes

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,3 +1,11 @@
 import express from "express"
 import { resiterUser, loginUser, getProfile } from "../controllers/authController"
 import { authMiddleware } from "../middleware/auth"
+
+const router = express.Router()
+
+router.post("/register", registerUser)
+router.post("/login", loginUser)
+router.post("/profile", authMiddleware, getProfile)
+
+export default router
