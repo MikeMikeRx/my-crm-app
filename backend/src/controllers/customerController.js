ee39bff4f6a5fd269a7d56327d42c7798f7b7188
MichalRais <Michal.Rais@iCloud.com>
2025-10-22 10:31:41 +0100

    feat: Implement getCustomers handler

--- backend/src/controllers/customerController.js
+++ backend/src/controllers/customerController.js
@@ -1,1 +1,10 @@
 import Customer from "../models/Customer"
+
+export const getCustomers = async (req, res, next) => {
+    try {
+        const customers = await Customer.find({ user: req.user.id }).sort({ createdAt: -1 })
+        res.json(customers)
+    } catch (err) {
+        next(err)
+    }
+}
