0306529bc8304224b2b1ce565441dd71d695bc18
MichalRais <Michal.Rais@iCloud.com>
2025-10-23 11:23:53 +0100

    feat: Define detailed Invoice Mongoose schema

--- backend/src/models/Invoice.js
+++ backend/src/models/Invoice.js
@@ -1,1 +1,47 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
+
+const invoiceSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        customer: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Customer",
+            required: true,
+        },
+        invoiceNumber: {
+            type: String,
+            required: [true, "Invoice number is requiered"],
+            unique: true,
+            trim: true,
+        },
+        issueDate: {
+            type: Date,
+            required: [true, "Issue date is required"]
+        },
+        dueDate : {
+            type: Date,
+            required: [true, "Due date is required"],
+        },
+        items: [
+            {
+                description: { type: String, required: true },
+                quantity: { type: Number, required: true, min: 1 },
+                unitPrice: { type: Number, required: true, min: 0 },
+            },
+        ],
+        status: {
+            type: String,
+            enum: ["draft", "sent", "paid", "overdue"],
+            default: "draft"
+        },
+        notes: {
+            type: String,
+            trim: true,
+        },
+    },
+    { timestamps: true }
+)
