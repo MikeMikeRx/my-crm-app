84aa81e673ead01ed7140b4f95cf7dfdada8fe8f
MichalRais <Michal.Rais@iCloud.com>
2025-10-23 14:08:55 +0100

    feat: Define and export Quote CRUD endpoint (JWT protected)

--- backend/src/routes/quote.js
+++ backend/src/routes/quote.js
@@ -7,3 +7,15 @@ import {
     updateQuote,
     deleteQuote,
 } from "../controllers/quoteController"
+
+const router = express.Router()
+
+router.use(authMiddleware)
+
+router.get("/", getQuotes)
+router.get("/:id", getQuoteById)
+router.post("/", createQuote)
+router.put("/:id", updateQuote)
+router.delete("/:id", deleteQuote)
+
+export default router
