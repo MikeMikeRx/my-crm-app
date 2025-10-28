95ed3606032155d6064cb60639eaa13b21707b5c
MichalRais <Michal.Rais@iCloud.com>
2025-10-28 15:43:59 +0000

    Fix: Add missing express-validator import in invoice routes

--- backend/src/routes/invoice.js
+++ backend/src/routes/invoice.js
@@ -1,4 +1,5 @@
 import express from "express"
+import { body } from "express-validator"
 import { authMiddleware } from "../middleware/auth.js"
 import { validateRequest } from "../middleware/validator.js"
 import {
