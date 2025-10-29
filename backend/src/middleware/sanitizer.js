bfbd06854f42cd193246465067be030744b59189
MichalRais <Michal.Rais@iCloud.com>
2025-10-29 12:52:21 +0000

    Chore: Replace xss-clean with custom sanitize-html middleware for Express 5

--- backend/src/middleware/sanitizer.js
+++ backend/src/middleware/sanitizer.js
@@ -1,12 +1,29 @@
-import xss from "xss-clean"
 import sanitize from "mongo-sanitize"
+import sanitizeHtml from "sanitize-html"
 
-export const sanitizeMiddleware = (app) => {
-    app.use(xss())
-    app.use((req, res, next) => {
-        if (req.body) req.body = sanitize(req.body)
-        if (req.params) req.params = sanitize(req.params)
-        if (req.query) req.query = sanitize(req.query)
+export const sanitizeMiddleware = (app) => {        
+    const sanitizeObject = (obj) => {
+        if(typeof obj !== "object" || obj === null) {
+            return res.status(400).json({ message: "Invalid company value"})
+        }
+
+        for (const key in obj) {
+            if (typeof obj[key] === "object") {
+                sanitizeObject(obj[key])
+            } else if (typeof obj[key] === "string") {
+                obj[key] = sanitizeHtml(obj[key], {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                })
+                obj[key] = sanitize(obj[key])
+            }
+        }
+    }
+
+    app.use((req, res, next) => {        
+        if (req.body) sanitizeObject(req.body)
+        if (req.params) sanitizeObject(req.params)
+        if (req.query) sanitizeObject(req.query)
         next()
     })
 }
