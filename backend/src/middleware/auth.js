a549b58cd72bde34d5405c263ebbeddf6ccaf52f
MichalRais <Michal.Rais@iCloud.com>
2025-10-30 17:03:59 +0000

    Refactor(auth): Improve JWT validation and user lookup in middleware

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -2,26 +2,30 @@ import jwt from "jsonwebtoken"
 import User from "../models/User.js"
 
 export const authMiddleware = async (req, res, next) => {
+    const authHeader = req.headers.authorization
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({ message: "No token provided, authorization denied"})
+    }
+
     try {
-        const authHeader = req.headers.authorization
-
-        if (!authHeader || !authHeader.startsWith("Bearer ")) {
-            return res.status(401).json({ message: "No token provided, authorization denied"})
-        }
-
         const token = authHeader.split(" ")[1]
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        if (!decoded?.id) {
+            return res.status(401).json({ message: "Invalid token payload." })
+        }
 
-        const user = await User.findById(decoded.id).select("-password")
+        const user = await User.findById(decoded.id).select("_id name email")
         if (!user) {
             return res.status(401).json({ message: "User not found or invalid token" })
         }
 
         req.user = user
+
         next()
     } catch (err) {
-        console.error("JWT vertification failed", err.message)
+        console.error("❌ JWT vertification failed", err.message)
         res.status(401).json({ message: "Token is not valid or expired" })
     }
 }
