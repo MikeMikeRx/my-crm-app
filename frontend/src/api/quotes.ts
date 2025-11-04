c39cd4b774486c0d577ee8762df13de5d8141cca
MichalRais <Michal.Rais@iCloud.com>
2025-11-04 12:30:24 +0000

    Feat(api): Add typed quote CRUD API functions

---
+++ frontend/src/api/quotes.ts
@@ -0,0 +1,27 @@
+import { api } from "./client";
+import type { Quote, QuoteCreate, QuoteUpdate, ID } from "@/types/entities";
+
+export async function listQuotes(): Promise<Quote[]> {
+    const { data } = await api.get<Quote[]>("/quotes");
+    return data;
+}
+
+export async function getQuote(id: ID): Promise<Quote> {
+    const { data } = await api.get<Quote>(`/quotes/${id}`);
+    return data;
+}
+
+export async function createQuote(payload: QuoteCreate): Promise<Quote> {
+    const { data } = await api.post("/quotes", payload);
+    return data;
+}
+
+export async function updateQuote(id: ID, payload: QuoteUpdate): Promise<Quote> {
+    const { data } = await api.put(`/quotes/${id}`, payload);
+    return data;
+}
+
+export async function deleteQuote(id: ID): Promise<{ message: string }> {
+    const { data } = await api.delete<{ message: string}>(`/quotes/${id}`);
+    return data;
+}
