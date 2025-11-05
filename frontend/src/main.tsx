f135e68089fb57842ec86c3ec1a37b0714ddb54f
MichalRais <Michal.Rais@iCloud.com>
2025-11-05 10:41:29 +0000

    Refactor(core): Initialize app with router and auth bootstrap in main entry

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 'antd/dist/reset.css'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { router } from "@/routes";
+import { useAuthStore } from "./context/authStore.ts";
+import "antd/dist/reset.css";
+import "@/index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function Bootsrap() {
+  const fetchProfile = useAuthStore((s) => s.fetchProfile);
+  React.useEffect(() => { fetchProfile(); }, [fetchProfile]);
+  return <RouterProvider router={router} />;
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Bootsrap />
+  </React.StrictMode>
+);
